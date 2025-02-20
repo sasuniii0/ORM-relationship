e058d87fab28a6d025c01239b51dbae4ae60e7c2
sasuniii0 <sasuniwijerathne@gmail.com>
2025-02-20 10:20:19 +0530

    day2

---
+++ One-to-One/src/main/java/lk/ijse/gdse/entity/Customer.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "customer_table")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @OneToOne
+    private IDCard idcard;
+}
---
+++ One-to-One/src/main/java/lk/ijse/gdse/entity/IDCard.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "nic_details")
+@Entity
+public class IDCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int nicNumber;
+    @OneToOne
+    private Customer customer;
+}
