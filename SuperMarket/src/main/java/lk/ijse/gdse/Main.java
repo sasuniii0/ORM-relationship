bf1b283acc92453cd605c45c2e4975ad9c146ce8
sasuniii0 <sasuniwijerathne@gmail.com>
2025-02-26 10:37:34 +0530

    get customer try

--- SuperMarket/src/main/java/lk/ijse/gdse/Main.java
+++ SuperMarket/src/main/java/lk/ijse/gdse/Main.java
@@ -1,11 +1,23 @@
 package lk.ijse.gdse;
 
 import lk.ijse.gdse.config.FactoryConfiguration;
+import lk.ijse.gdse.entity.Customer;
+import lk.ijse.gdse.entity.Order;
 import org.hibernate.Session;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Session session = FactoryConfiguration.getInstance().getSession();
+
+        Customer customer = session.get(Customer.class, 1);
+        System.out.println(customer);
+
+        List<Order> orders = customer.getOrders(); // <Order>
+        for (Order order : orders) {
+            System.out.println(order);
+        }
         session.close();
     }
 }
