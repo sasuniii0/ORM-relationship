5c08eb9b55953e216bcfe25be64c7c2bf8748243
sasuniii0 <sasuniwijerathne@gmail.com>
2025-03-05 09:23:48 +0530

    create query db

--- Query/src/main/java/lk/ijse/gdse/Main.java
+++ Query/src/main/java/lk/ijse/gdse/Main.java
@@ -1,11 +1,23 @@
 package lk.ijse.gdse;
 
 import lk.ijse.gdse.config.FactoryConfiguration;
+import lk.ijse.gdse.entity.Customer;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Session session = FactoryConfiguration.getInstance().getSession();
+        Query<Customer> query = session.createQuery("from Customer", Customer.class);
+        List<Customer> customer = query.list();
+
+        for (Customer customer1 : customer) {
+            System.out.println(customer1.getId());
+            System.out.println(customer1.getName());
+            System.out.println(customer1.getEmail());
+        }
         session.close();
     }
 }
